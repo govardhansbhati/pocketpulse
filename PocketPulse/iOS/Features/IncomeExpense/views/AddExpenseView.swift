ec2f826eda00746555be44389934eaa2bf8d53a5
govardhansbhati <mailtogovardhansinghbhati@gmail.com>
2025-07-26 21:20:39 +0530

    .

--- PocketPulse/iOS/Features/IncomeExpense/views/AddExpenseView.swift
+++ PocketPulse/iOS/Features/IncomeExpense/views/AddExpenseView.swift
@@ -37,11 +37,6 @@ struct AddExpenseView: View {
                     TextField("Amount", text: $viewModel.amount)
                         .keyboardType(.decimalPad)
 
-//                    Picker("Type", selection: $viewModel.type) {
-//                        Text("Expense").tag(TransactionType.expense)
-//                        Text("Income").tag(TransactionType.income)
-//                    }
-
                     Picker("Category", selection: $viewModel.category) {
                         ForEach(TransactionCategory.allCases) { category in
                             Text(category.displayName).tag(category)
